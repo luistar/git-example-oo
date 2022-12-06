07807952e67e5d65dd2e6529b3ffe76b10c1685e
Luigi Starace <luigi.starace@gmail.com>
2022-12-06 10:22:08 +0100

    Modificato nome somma e aggiunto metodo sottrazione

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,5 +1,9 @@
 public class Calcolatrice {
-    public static int somma(int a, int b){
+    public static int sum(int a, int b){
         return a+b;
+    }
+
+    public static int subtract(int a, int b){
+        return a-b;
     }
 }
