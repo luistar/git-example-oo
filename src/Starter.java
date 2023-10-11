ca5dca8e496cdb6671164c8545f45134fc8bf5dd
sergi <sergi@XPS23>
2023-10-11 15:51:37 +0200

    Modifica

--- src/Starter.java
+++ src/Starter.java
@@ -8,7 +8,7 @@ public class Starter {
 		
 		Esame e1 = new Esame("N86001234", "Algebra", 26, false);
 		Esame e2 = new Esame("N86001234", "Programmazione", 28, false);
-		Esame e3 = new Esame("N86001234", "Geometria", 26, true);
+
 
 		
 //		System.out.println(e1);
